f7427df6a2dab650fa4aab43b434215bd6803f0c
Dev Glenox <025glenox025@gmail.com>
2020-07-16 14:36:20 +0800

    init

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * PHP versions 7.0+
+ *
+ * Kanin.PHP(tm) : Application Framework
+ * Copyright 2020, Indevfinite Software Foundation, Inc. (https:/indevfinite.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2020, Indevfinite Software Foundation, Inc. (https:/indevfinite.com)
+ * @link          Kanin.PHP Project
+ * @package       Kanin
+ * @subpackage    Kanin Appplication framework
+ * @since         Kanin.PHP v0.0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+
+ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+     return false;
+}
+ require 'public' . DIRECTORY_SEPARATOR . 'index.php';
---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHP versions 7.0+
+ *
+ * Kanin.PHP(tm) : Application Framework
+ * Copyright 2020, Indevfinite Software Foundation, Inc. (https:/indevfinite.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2020, Indevfinite Software Foundation, Inc. (https:/indevfinite.com)
+ * @link          Kanin.PHP Project
+ * @package       Kanin
+ * @subpackage    Kanin Appplication framework
+ * @since         Kanin.PHP v0.0.1
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+
+
